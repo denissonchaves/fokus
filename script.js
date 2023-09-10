44ebc03cba9d0ac8f71d1a689e00e2f7eec7ce1d
Denisson Chaves <denisson.chaves@hotmail.com>
2023-09-10 04:00:42 -0300

    feat: add play, pause and end sounds

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const botoes = document.querySelectorAll(".app__card-button");
 const startPauseBt = document.querySelector("#start-pause");
 const musicaFocoInput = document.querySelector("#alternar-musica");
 const musica = new Audio("./sons/luna-rise-part-one.mp3");
+const audioTempoFinalizado = new Audio("./sons/beep.mp3");
+const audioPlay = new Audio("./sons/play.wav");
+const audioPause = new Audio("./sons/pause.mp3");
+
 musica.loop = true;
 musicaFocoInput.checked = false;
 
@@ -72,6 +76,7 @@ longoBt.addEventListener("click", () => {
 
 const contagemRegressiva = () => {
   if (tempoDecorridoEmSegundos <= 0) {
+    audioTempoFinalizado.play();
     alert("Tempo finalizado!");
     zerar();
     return;
@@ -85,9 +90,11 @@ startPauseBt.addEventListener("click", iniciarOuPausar);
 
 function iniciarOuPausar() {
   if (intervaloId) {
+    audioPause.play();
     zerar();
     return;
   }
+  audioPlay.play();
   intervaloId = setInterval(contagemRegressiva, 1000);
 }
 
